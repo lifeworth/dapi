086519b4f2ad76cc3a589f99ac416278aed33bfc
oneppiece <164408623@qq.com>
2022-08-26 13:05:20 +0800

    新增redis Test

--- src/test/java/com/duzy/RedisTest.java
+++ src/test/java/com/duzy/RedisTest.java
@@ -1,6 +1,9 @@
 package com.duzy;
 
+import cn.hutool.core.util.RandomUtil;
 import cn.hutool.json.JSONUtil;
+import com.duzy.model.UserModel;
+import com.duzy.repository.UserRepository;
 import com.google.common.collect.Lists;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -9,9 +12,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -25,6 +30,9 @@ import java.util.concurrent.TimeUnit;
 public class RedisTest {
     @Autowired
     StringRedisTemplate stringRedisTemplate;
+
+    @Autowired
+    UserRepository userRepository;
 
     String valueKeyPrefix = "test";
     String hashKeyPrefix = "hash:";
@@ -42,7 +50,7 @@ public class RedisTest {
     }
 
     @Test
-    public void value() {
+    public void template() {
         stringRedisTemplate.opsForValue().set(valueKeyPrefix, "test中文", 10, TimeUnit.SECONDS);
         stringRedisTemplate.opsForHash().put(hashKeyPrefix, "1", JSONUtil.toJsonStr(map));
         stringRedisTemplate.opsForList().leftPushAll(listKeyPrefix, keys);
@@ -59,5 +67,23 @@ public class RedisTest {
         System.out.println(bit123);
         System.out.println(bit456);
 
+    }
+
+    @Test
+    public void repository() {
+        UserModel model = new UserModel();
+        int id = RandomUtil.randomInt();
+        model.setId(id);
+        model.setUsername(RandomUtil.randomString("usernameusername", 8));
+        model.setNick(RandomUtil.randomString("nicknicknick", 8));
+        model.setPassword(RandomUtil.randomString("passwordpasswordpassword", 8));
+        model.setPhone(RandomUtil.randomString("1234567890123456789012345678901234567", 8));
+        model.setCreatedBy("test");
+        model.setCreatedTime(LocalDateTime.now());
+        model.setUpdatedBy("test");
+        model.setUpdatedTime(LocalDateTime.now());
+        userRepository.save(model);
+        Optional<UserModel> modelFromRedisRepository = userRepository.findById(id);
+        System.out.println(modelFromRedisRepository.get());
     }
 }
