70a52a9efa2d6378eb5661f9bb4ae8ccbaf6c0ad
duzhiyuan <duzhiyuan@haohaozhu.com>
2023-01-07 17:08:50 +0800

    设置时区

--- src/main/resources/db/migration/V1.0/V20230107.1__table.sql
+++ src/main/resources/db/migration/V1.0/V20230107.1__table.sql
@@ -1,2 +1,12 @@
-ALTER TABLE ssh_log MODIFY COLUMN create_time datetime DEFAULT CURRENT_TIMESTAMP NOT NULL;
-ALTER TABLE ssh_log MODIFY COLUMN update_time datetime DEFAULT CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP NOT NULL;
+alter table dapi.ip_location add created_time datetime null comment '创建时间';
+alter table dapi.ip_location add updated_time datetime null comment '更新时间';
+alter table dapi.ip_location add created_by varchar(32)  null comment '创建人'
+alter table dapi.ip_location add updated_by varchar(32) null comment '更新人';
+
+alter table dapi.ssh_log add created_time datetime null comment '创建时间';
+alter table dapi.ssh_log add updated_time datetime null comment '更新时间';
+alter table dapi.ssh_log add created_by varchar(32)  null comment '创建人'
+alter table dapi.ssh_log add updated_by varchar(32) null comment '更新人';
+
+ALTER TABLE ssh_log MODIFY COLUMN created_time datetime DEFAULT CURRENT_TIMESTAMP NOT NULL;
+ALTER TABLE ssh_log MODIFY COLUMN updated_time datetime DEFAULT CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP NOT NULL;
