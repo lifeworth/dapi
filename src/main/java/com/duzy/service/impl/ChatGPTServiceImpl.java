61c1d117b3f408acc5fd22c9020d9f7b76f9e888
zhiyuandu <duzhiyuan@haohaozhu.com>
2023-02-10 14:23:19 +0800

    添加chat

--- src/main/java/com/duzy/service/impl/ChatGPTServiceImpl.java
+++ src/main/java/com/duzy/service/impl/ChatGPTServiceImpl.java
@@ -7,6 +7,7 @@ import com.theokanning.openai.completion.CompletionChoice;
 import com.theokanning.openai.completion.CompletionRequest;
 import com.theokanning.openai.model.Model;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -19,6 +20,10 @@ import java.util.List;
 @Service
 @Slf4j
 public class ChatGPTServiceImpl implements ChatGPTService {
+
+    @Value("${chat-token}")
+    String token;
+
     /**
      * 获取模型
      * @return
@@ -30,14 +35,14 @@ public class ChatGPTServiceImpl implements ChatGPTService {
 
     @Override
     public String qa(String question) {
-        OpenAiService service = new OpenAiService("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");
+        OpenAiService service = new OpenAiService(token);
         CompletionRequest completionRequest = CompletionRequest.builder()
                 .prompt(question)
                 .model("ada")
                 .echo(true)
                 .build();
         List<CompletionChoice> choices = service.createCompletion(completionRequest).getChoices();
-        log.info("收到:{},回答:{},",question, choices);
+        log.info("收到:{},回答:{},", question, choices);
         return JSONUtil.toJsonStr(choices);
     }
 }
