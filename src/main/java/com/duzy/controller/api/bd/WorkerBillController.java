17bd1a9ccc68b19332bd52cb00759b7b38c33712
杜致远 <duzhiyuan@1.com>
2025-04-28 14:46:15 +0800

    重构Excel导出逻辑并调整WorkerBillServiceImpl常量值

--- src/main/java/com/duzy/controller/api/bd/WorkerBillController.java
+++ src/main/java/com/duzy/controller/api/bd/WorkerBillController.java
@@ -1,8 +1,5 @@
 package com.duzy.controller.api.bd;
 
-import cn.hutool.core.io.IoUtil;
-import cn.hutool.poi.excel.ExcelUtil;
-import cn.hutool.poi.excel.ExcelWriter;
 import com.duzy.controller.CustomerController;
 import com.duzy.dto.WorkerBillDto;
 import com.duzy.dto.query.WorkerBillQueryDto;
@@ -11,7 +8,6 @@ import com.duzy.service.WorkerBillService;
 import com.duzy.vo.WorkerBillExportVo;
 import com.duzy.vo.WorkerBillVo;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.servlet.ServletOutputStream;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,6 +16,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 import java.util.List;
+
+import static com.duzy.common.util.FileUtil.exportList;
 
 /**
  * <p>
@@ -43,17 +41,10 @@ public class WorkerBillController extends CustomerController<WorkerBillModel, Wo
 
         List<WorkerBillExportVo> listByParentName = service.getListByParentName();
 
-        ExcelWriter writer = ExcelUtil.getWriter(true);
-        writer.write(listByParentName, true);
-
-        response.setContentType("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=utf-8");
-        response.setHeader("Content-Disposition", "attachment;filename=test.xlsx");
-        ServletOutputStream out = response.getOutputStream();
-
-        writer.flush(out, true);
-        writer.close();
-        IoUtil.close(out);
+        exportList(response, listByParentName);
 
 
     }
+
+
 }
